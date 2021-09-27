b57361782c634904e761f665e62e8b087302c570
Samuele Leone <samueleartisanleone@gmail.com>
2021-09-27 21:56:11 +0200

    prova

--- pages/api/horoscope.js
+++ pages/api/horoscope.js
@@ -4,7 +4,7 @@ const moment = require('moment');
 const cheerio = require('cheerio');
 const expressPrettier = require('express-prettier');
 var today = moment().locale('it').format('DD-MMMM-YYYY');
-var today_pretty = moment().locale('it').format('DD MMMM YYYY');
+var today_pretty =  moment().locale('it').format('DD MMMM YYYY');
 var every_thursday = moment().day(-3).locale('it').format('DD');
 var every_next_wednesday = moment().day(+3).locale('it').format('DD');
 var year = moment().year();
@@ -16,42 +16,63 @@ const BASE_URL = 'https://www.internazionale.it/oroscopo/';
 
 
 export default function handler(req, res) {
-    // The scraping magic will happen here
-    let sign = req.query.sign;
-
-    let url = BASE_URL + last_thursday + '/' + sign + '-' + every_thursday + '-' + every_next_wednesday + '-' + month_string + '-' + year;
-    res.setHeader('content-type', 'application/json');
-
-    request(url, function (error, response, body) {
+      // The scraping magic will happen here
+      let sign = req.query.sign;
+      let json = '';
+      let url = BASE_URL + last_thursday + '/' + sign + '-' + every_thursday + '-' + every_next_wednesday + '-' + month_string + '-' + year;
+      res.setHeader('content-type', 'application/json');
+     
+      request(url, function (error, response, body) {
 
         // Magic happens here
-        if (sign != null) {
+        if ( sign != null || sign == '') {
             var $ = cheerio.load(body);
             var prediction = $('div.item_text > p').text();
+            if (prediction == '') {
+                if (sign == ''){
+                    sign = 'null'
+                }
+                var json_error = {
+                    sign: sign,
+                    text: ' No messages from stars for '+sign,
+                    error_code : ' 404 ',
+                    error_desc : ' Sign not found '
+                }
+                
+                json_error = JSON.stringify(json_error, null, 4);
+                json = json_error;
+                res.statusCode = 404;
+                
 
+            } else {
+                
+                var json = {
+                    sign: sign,
+                    text: prediction,
+                    start_date: every_thursday + ' ' + month_string + ' ' + year,
+                    end_date: every_next_wednesday + ' ' + month_string + ' ' + year,
+                };
 
-            var json = {
-                sign: sign,
-                text: prediction,
-                start_date: every_thursday + ' ' + month_string + ' ' + year,
-                end_date: every_next_wednesday + ' ' + month_string + ' ' + year,
-            };
+                // Send the JSON as a response to the client
+                
+                json = JSON.stringify(json, null, 4);
+                // Send the JSON as a response to the client
+                res.statusCode = 200;
+                
+                
 
-            // Send the JSON as a response to the client
-
-            json = JSON.stringify(json, null, 4);
-            // Send the JSON as a response to the client
-            res.statusCode = 200;
-            return res.send(json);
-            
-
-
-
+                
+            }
+        }else{
+          res.statusCode =200;
+          json = 'Welcome to endpoint, to get started just add parameters to GET request.';
         }
-        res.statusCode = 404;
-        return res.send('Welcome to rest API endpoint, to getting start read documentation.')
+        return res.send(json);
+        
+        res.end();
+      
 
-    res.end();
+       
     });
 }
 
