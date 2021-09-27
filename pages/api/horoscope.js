6e25a7015435ee5af4c168bf2cd81e8613eafd26
Samuele Leone <samueleartisanleone@gmail.com>
2021-09-27 23:19:04 +0200

    new

--- pages/api/horoscope.js
+++ pages/api/horoscope.js
@@ -12,39 +12,42 @@ var month = '0' + (moment().month() + 1);
 var month_string = moment().locale('it').format('MMMM');
 var last_thursday = moment().day(-3).locale('it').format('YYYY/MM/DD');
 const BASE_URL = 'https://www.internazionale.it/oroscopo/';
+//var prediction = $('div.item_text > p').text();
+var wordOfDay = [];
 
 
-
-export default async function handler(req, res) {
+export default  function handler(req, res) {
       // The scraping magic will happen here
       let sign = req.query.sign;
-      let json = '';
       let url = BASE_URL + last_thursday + '/' + sign + '-' + every_thursday + '-' + every_next_wednesday + '-' + month_string + '-' + year;
       res.setHeader('content-type', 'application/json');
-      
-     
-      var r = request(url, async function (error, response, body) {
-        var $ = await cheerio.load(body);
-        var prediction = $('div.item_text > p').text();
-        console.log(prediction);
-        var json = {
-            sign: sign,
-            text: prediction,
-            start_date: every_thursday + ' ' + month_string + ' ' + year,
-            end_date: every_next_wednesday + ' ' + month_string + ' ' + year,
-        };
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With');
+      console.log(url);
+      request({
+        method: 'GET',
+        url: url,
 
-        // Send the JSON as a response to the client
-        
-        json = JSON.stringify(json, null, 4);
-        res.send(json);
-        res.statusCode = 200;
-        res.end();
-       });
-
-       console.log(r);
+      }, function(err, response, body, callback) {
+          if (err) return console.error(err);
+          
+          // get the HTML body from WordThink.com
+          var $ = cheerio.load(body);
     
-  
+          if(wordOfDay.length > 0){
+            wordOfDay = [];
+          }
+    
+          var post = $('h2.hentry__title > a ').text();
+          
+          // create an object
+          wordOfDay.push(post);
+          res.send(wordOfDay);
+    
+      });
+      
+    
+     
     
 }
 
