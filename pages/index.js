3a70d1a31ff884e3299b5d332e6291fdaafb0371
Samuele Leone <44635579+wiztoth@users.noreply.github.com>
2021-09-28 16:51:27 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         
         <h1 className={styles.title}>
-          wizoscopo API &#10024;
+          wizapi &#10024;
         </h1>
 
        
