997b35b214bcf54e7eb458bf963af538ac1073dc
Samuele Leone <44635579+wiztoth@users.noreply.github.com>
2021-09-28 16:52:46 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         
         <h1 className={styles.title}>
-          wizAPI &#10024;
+          wiz API &#10024;
         </h1>
 
        
