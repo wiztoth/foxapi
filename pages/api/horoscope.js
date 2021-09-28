343e4b7385eeb04454c0b2d22df0dd47a662994f
Samuele Leone <leone@netstudio.it>
2021-09-28 11:51:35 +0200

    new

--- pages/api/horoscope.js
+++ pages/api/horoscope.js
@@ -8,8 +8,8 @@ export default async function handler(req, res) {
 
   var sign = req.query.sign;
  
-  if(sign != null){
-    const url = 'https://www.internazionale.it/oroscopo/'+start_date+'/'+sign+'-'+first_day_week+'-'+end_date;
+ 
+  const url = 'https://www.internazionale.it/oroscopo/'+start_date+'/'+sign+'-'+first_day_week+'-'+end_date;
  
   const request = await fetch(url);
   var body = await request.text();
@@ -17,17 +17,9 @@ export default async function handler(req, res) {
   var result = $('h2.hentry__title').text();
   var prediction = $('div.item_text > p ').text();
 
-  var json = {
-    sign : sign,
-    message: prediction,
-    start_date: start_date,
-    end_date: end_date
-  }
- 
-  res.send(json);
-}else{
-  res.send(' nessun segno immesso ');
-}
+  
+  res.send(prediction);
+
     
  
   
