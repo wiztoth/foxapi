8e71e7076acbd30856fc712202d0d598b2ae0845
Samuele Leone <leone@netstudio.it>
2021-09-28 11:43:08 +0200

    new think

--- pages/api/horoscope.js
+++ pages/api/horoscope.js
@@ -1,15 +1,50 @@
 const cheerio = require('cheerio');
+const moment = require('moment');
+var start_date = moment().day(-3).locale('it').format('YYYY/MM/DD');
+var first_day_week = moment().day(-3).locale('it').format('DD');
+var end_date = moment().day(+3).locale('it').format('DD-MMMM-YYYY');
 
 export default async function handler(req, res) {
 
-
-
-  const request = await fetch(`https://www.internazionale.it/oroscopo/2021/09/23/cancro-23-29-settembre-2021`);
+  var sign = req.query.sign;
+ 
+  if(sign != null){
+    const url = 'https://www.internazionale.it/oroscopo/'+start_date+'/'+sign+'-'+first_day_week+'-'+end_date;
+ 
+  const request = await fetch(url);
   var body = await request.text();
   const $ = cheerio.load(body);
-  var title = $('title').text();
-  console.log(title);
-  res.send(title);
+  var result = $('h2.hentry__title').text();
+  var prediction = $('div.item_text > p ').text();
+
+  if(result != 'Pagina non trovata'){
+    var json = {
+      sign : sign,
+      message: prediction,
+      start_date: start_date,
+      end_date: end_date
+    }
+   
+    res.send(JSON.stringify(json, null, 4));
+    res.statusCode = 200;
+    res.end();
+  }else{
+    var error = {
+      sign : sign,
+      message: 'No messages from stars for '+sign,
+      error_code: '404',
+      error_description: sign+' not found'
+    }
+    res.send(JSON.stringify(error, null, 4));
+    res.statusCode = 404;
+    res.end();
+  }
+  }else{
+    res.send(' Welcome to endpoint, add parameter to start as described in documentation');
+  }
+  
+
+  
 }
 
 
